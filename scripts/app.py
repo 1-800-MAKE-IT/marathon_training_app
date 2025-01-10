3ebf70127972c13271ed84ecc229fb1eb05a1c34
1-800-MAKE-IT <archiegoodman2011@gmail.com>
2025-01-10 14:14:39 +0000

    basic bugfix

--- scripts/app.py
+++ scripts/app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import logging
-from scripts.chatbot import get_response  # Import chatbot logic
-from scripts.data_storage import save_user_data, get_user_data  # Import data management logic
+#from scripts.chatbot import get_response  # Import chatbot logic
+#from scripts.data_storage import save_user_data, get_user_data  # Import data management logic
 
 
 logging.basicConfig(
@@ -15,7 +15,7 @@ logging.basicConfig(
 
 #set title and add disclaimer
 st.set_page_config(page_title="Training Buddy", layout="centered")
+st.title("Marathon Training App")
 st.markdown("An AI-powered tool to help you plan, track, and optimize your marathon training journey!.")
 st.markdown("IMPORTANT NOTE: Please do not enter ANY Personally Identifiable Information (PII). When prompted for a username, please ensure to use an anonymised one, and never enter ANY sensitive data.")
 
-st.title
