267b79c582d9f2d5928cf0a6bd4bff6df1f2ed37
1-800-MAKE-IT <archiegoodman2011@gmail.com>
2025-01-16 18:19:31 +0000

    bugfix on main location

--- Home.py
+++ Home.py
@@ -48,7 +48,7 @@ st.markdown(page_bg_img, unsafe_allow_html=True)
 
 authenticator = get_authenticator()
 
-name, status, user = authenticator.login("Login", "main")
+name, status, user = authenticator.login('Login', 'main')
 if status:
     st.success(f"Welcome {name}")
 elif status is False:
