702340f6d90ed38ce519560abbdd85f351440ceb
1-800-MAKE-IT <92301571+1-800-MAKE-IT@users.noreply.github.com>
2025-02-10 22:09:28 +0000

    added logic to create chroma db

--- scripts/data_storage.py
+++ scripts/data_storage.py
@@ -1,8 +1,16 @@
-import logging
+# from langchain.document_loaders import DirectoryLoader
 from langchain_community.document_loaders import DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.schema import Document
-from typing import List
+# from langchain.embeddings import OpenAIEmbeddings
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import Chroma
+import openai 
+from dotenv import load_dotenv
+import os
+import shutil
+import logging
+import types
 
 logging.basicConfig(
     level=logging.INFO,
@@ -13,7 +21,7 @@ logging.basicConfig(
     ]
 )
 
-def load_documents(data_path: str = "data/PDFs") -> List[Document] :
+def load_documents(data_path: str = "data/PDFs") -> list[Document] :
     """
     Load documents into Document datatype in langchain. Document also includes metadata.
     TO DO - add additional metadata from json file.
@@ -25,17 +33,17 @@ def load_documents(data_path: str = "data/PDFs") -> List[Document] :
     Documents (list of langchain document objects) : documents and metadata
     """
     loader = DirectoryLoader(data_path, glob="*.pdf")
-    documents: List[Document] = loader.load()
+    documents: list[Document] = loader.load()
     
     logging.info(f'''Downloaded documents with metadata: {documents.metadata}''')
 
     return documents
 
-def split_text() -> List[Document] :
+def split_text() -> list[Document] :
     """
-    Gets chunks
+    Gets chunks from documents
     """
-    documents: List[Document] = load_documents(data_path="data/PDFs")
+    documents: list[Document] = load_documents(data_path="data/PDFs")
 
     chunk_size: int = 1000
     chunk_overlap: int = 500
@@ -47,21 +55,31 @@ def split_text() -> List[Document] :
         add_start_index=True
     )
 
-    chunks: List[Document] = text_splitter.split_documents(documents)
+    chunks: list[Document] = text_splitter.split_documents(documents)
 
     logging.info(f'''Downloaded {len(documents)} documents and split into {len(chunks)}. Chunk_size was {chunk_size} and chunk overlap {chunk_overlap}''')
 
     return chunks
 
-def save_to_chroma() -> int :
-    
-    
+def save_to_chroma(chunks : list[Document]) -> int :
+    """
+    Creates chroma DB and saves chunks from documents
+    """
+
     chroma_path : str = "data/chroma"
 
+    if os.path.exists(chroma_path):
+        shutil.rmtree(chroma_path)
 
     logging.info(f'''Created and populated chroma db at path {chroma_path} ''')
 
-    
+    db = Chroma.from_documents(
+        chunks, OpenAIEmbeddings(), persist_directory=chroma_path
+    )
 
+    #save database
+    db.persist()
+
+    logging.info(f"Saved {len(chunks)} chunks to {chroma_path}.")
 
     return 1
