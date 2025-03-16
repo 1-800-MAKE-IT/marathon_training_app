2f743a1ab503e54d84db7441963258cab1053648
1-800-MAKE-IT <92301571+1-800-MAKE-IT@users.noreply.github.com>
2025-03-16 20:34:02 +0000

    improved error handling

--- pages/1_Personal_Coach.py
+++ pages/1_Personal_Coach.py
@@ -88,7 +88,7 @@ if st.session_state["authentication_status"]:
         st.session_state["messages"].append({"role": "user", "content": query})
 
         if response != "Complete":
-            st.error("Error: Unable to fetch data. Please try again.")
+            st.error(str(response))
         else:
             # Call model and get response
             error_code, bot_response = query_vector_db(query)  
